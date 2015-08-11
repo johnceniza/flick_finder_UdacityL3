9307087413e37358eb7539cd58596aad3464a540
John Ceniza <jc@johnceniza.com>
2015-08-11 17:59:07 -0400

    initial commit - layed out UI in Storyboard

--- flick_finder_JC/ViewController.swift
+++ flick_finder_JC/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theImage: UIImageView!
+    @IBOutlet weak var phraseField: UITextField!
+    @IBOutlet weak var longField: UITextField!
+    @IBOutlet weak var latField: UITextField!
+    @IBOutlet weak var imageTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func phraseSearchPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func latLongSearchPressed(sender: UIButton) {
+        
+    }
 
 }
 
