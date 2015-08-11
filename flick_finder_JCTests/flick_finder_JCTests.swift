d8ff870944653e77cbffa37a468e8f3f2503fa06
John Ceniza <jc@johnceniza.com>
2015-08-11 17:51:02 -0400

    Initial Commit

---
+++ flick_finder_JCTests/flick_finder_JCTests.swift
@@ -0,0 +1,36 @@
+//
+//  flick_finder_JCTests.swift
+//  flick_finder_JCTests
+//
+//  Created by John Ceniza on 8/11/15.
+//  Copyright (c) 2015 AppDeco. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class flick_finder_JCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
